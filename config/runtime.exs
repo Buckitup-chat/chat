55d86d69be55aece373fdd2962349185aa2f380e
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-03-20 15:58:29 +0200

    url config

--- config/runtime.exs
+++ config/runtime.exs
@@ -35,7 +35,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :chat, ChatWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [host: host, port: 443, schema: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.

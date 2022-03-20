0768ae0884a997b91000068dc5df9dc969d522f8
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-03-20 15:46:41 +0200

    config fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -28,7 +28,7 @@ if config_env() == :prod do
   host =
     System.get_env("PHX_HOST") ||
       if System.get_env("APP_NAME") do
-        "#{System.get_env("APP_NAME")}.gigalixir.com"
+        "#{System.get_env("APP_NAME")}.gigalixirapp.com"
       end ||
       "buckitup.net"
 

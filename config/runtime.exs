68b7122fe6f338428ccd9b9407900e8e4af6c8d4
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-03-20 15:34:07 +0200

    config fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -25,7 +25,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host =
+    System.get_env("PHX_HOST") ||
+      if System.get_env("APP_NAME") do
+        "#{System.get_env("APP_NAME")}.gigalixir.com"
+      end ||
+      "buckitup.net"
+
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :chat, ChatWeb.Endpoint,

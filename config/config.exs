73909b17e09f1d35011629cd1996484ee03eeb54
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-04-22 12:26:25 +0300

    disable timezone autoupdate

--- config/config.exs
+++ config/config.exs
@@ -46,6 +46,7 @@ config :phoenix, :json_library, Jason
 
 # Timezone config
 config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
+config :tzdata, :autoupdate, :disabled
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.

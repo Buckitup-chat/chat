fc1558b884aad89ab7982c9f0a0bbe1a73ec2d19
Serhii Lukianov <serhii.lukianov@litslink.com>
2022-08-12 15:39:37 +0300

    suspending TLS errors in logs

--- test/chat/ordering_test.exs
+++ test/chat/ordering_test.exs
@@ -3,8 +3,11 @@ defmodule Chat.OrderingTest do
 
   alias Chat.Card
   alias Chat.Dialogs
+  alias Chat.Messages
   alias Chat.Ordering
+  alias Chat.Rooms
   alias Chat.User
+  alias Chat.Utils
 
   test "should provide 1 on new key" do
     assert 1 = Ordering.next({:some, "key"})
@@ -16,6 +19,18 @@ defmodule Chat.OrderingTest do
     assert 1 < Ordering.next(dialog_key_preix)
   end
 
+  test "should provide next key on existing room message" do
+    room_msg_prefix = prefix_for_room_with_5_msgs()
+
+    assert 5 = Ordering.last(room_msg_prefix)
+    assert 6 = Ordering.next(room_msg_prefix)
+
+    Ordering.reset()
+
+    assert 5 = Ordering.last(room_msg_prefix)
+    assert 6 = Ordering.next(room_msg_prefix)
+  end
+
   defp messaged_dialog_key_prefix do
     user = User.login("some")
 
@@ -25,5 +40,24 @@ defmodule Chat.OrderingTest do
     |> Dialogs.add_new_message(user, dialog)
 
     {:dialog_message, dialog |> Dialogs.Dialog.dialog_key()}
+  end
+
+  defp prefix_for_room_with_5_msgs do
+    alice = User.login("Alice")
+    alice |> User.register()
+
+    room_identity = alice |> Rooms.add("some room")
+    room = Rooms.Room.create(alice, room_identity)
+
+    message = "hello, room  "
+
+    for num <- 1..5 do
+      message
+      |> Kernel.<>(to_string(num))
+      |> Messages.Text.new(num)
+      |> Rooms.add_new_message(alice, room.pub_key)
+    end
+
+    {:room_message, room.pub_key |> Utils.binhash()}
   end
 end

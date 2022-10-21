5e576b75be38fbc50f9946beb38c249d069c97b8
Ruslan Kurbanov <ruslankurbanov@MacBook-Pro-lits-admin.local>
2022-10-21 12:37:25 +0300

    filling icons with red color

--- lib/chat_web/live/main_live/layout/db_status.ex
+++ lib/chat_web/live/main_live/layout/db_status.ex
@@ -8,35 +8,29 @@ defmodule ChatWeb.MainLive.Layout.DbStatus do
   def desktop(assigns) do
     ~H"""
       <button  class="sidebarIcon mb-1">
-        <%= if @status.writable == :no do %>
-          <!-- Red background on full button -->
-            <div class="ml-6 mb-2">
-              <.icon id="redDataBase" class="w-6 h-6"/>
-            </div>
-        <% end %>
 
         <%= if @status.mode == :internal do %>
           <!-- Crossed DB icon -->
-            <div class="ml-[23.5px] pb-2.5 pr-1">
-              <.icon id="crossedDataBase" class={classes("w-6 h-6 fill-gray-100", %{"fill-red-600" => @status.writable == :no})}/>
+            <div class="ml-[23.5px] pb-2.5">
+              <.icon id="crossedDataBase" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
         <%= if @status.mode == :main do %>
           <!-- DB icon -->
             <div class="ml-5 mb-2">
-              <.icon id="dataBase" class="w-6 h-6"/>
+              <.icon id="dataBase" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
         <%= if @status.mode == :internal_to_main do %>
           <!-- Blinking or transparent DB icon -->
             <div class="ml-5 mb-2">
-              <.icon id="dataBase" class="w-6 h-6 animation-pulse"/>
+              <.icon id="dataBase" class={classes("w-6 h-6 fill-gray-200 animation-pulse", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
         <%= if @status.mode == :main_to_internal do %>
           <!-- Blinking or transparent Crossed DB icon -->
             <div class="ml-5 mb-2">
-              <.icon id="crossedDataBase" class="w-6 h-6 animation-pulse"/>
+              <.icon id="crossedDataBase" class={classes("w-6 h-6 fill-gray-200 animation-pulse", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
 
@@ -44,17 +38,17 @@ defmodule ChatWeb.MainLive.Layout.DbStatus do
           <!-- Saving or downloading icon -->
           <%= if @status.flags[:backup] do %>
           <div class="ml-[28px] mb-1">
-            <.icon id="backUp" class="w-6 h-6"/>
+            <.icon id="backUp" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
           </div>
           <% end %>
           <!-- Syncronization icon, like cirle arrows -->
           <%= if @status.flags[:replication] do %>
           <div class="ml-[28px]">
-            <.icon id="replication" class="w-6 h-6"/>
+            <.icon id="replication" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
           </div>
            <% end %>
             <!--<div class="ml-7">
-              <.icon id="car" class="w-6 h-6"/>
+              <.icon id="car" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
             </div>-->
       </button>
     """
@@ -63,35 +57,28 @@ defmodule ChatWeb.MainLive.Layout.DbStatus do
   def mobile(assigns) do
     ~H"""
       <div  class="flex flex-row items-center pt-2 w-[63%] justify-end">
-        <%= if @status.writable == :no do %>
-          <!-- Red background on full button -->
-            <div>
-              <.icon id="redDataBase" class="w-6 h-6"/>
-            </div>
-        <% end %>
-
         <%= if @status.mode == :internal do %>
           <!-- Crossed DB icon -->
             <div class="pb-2 pr-1">
-              <.icon id="crossedDataBase" class="w-5 h-[19.5px] bg-red-600"/>
+              <.icon id="crossedDataBase" class={classes("w-5 h-[19.5px] fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
         <%= if @status.mode == :main do %>
           <!-- DB icon -->
             <div>
-              <.icon id="dataBase" class="w-6 h-6"/>
+              <.icon id="dataBase" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
         <%= if @status.mode == :internal_to_main do %>
           <!-- Blinking or transparent DB icon -->
             <div>
-              <.icon id="dataBase" class="w-6 h-6 animation-pulse"/>
+              <.icon id="dataBase" class={classes("w-6 h-6 fill-gray-200 animation-pulse", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
         <%= if @status.mode == :main_to_internal do %>
           <!-- Blinking or transparent Crossed DB icon -->
             <div>
-              <.icon id="crossedDataBase" class="w-6 h-6 animation-pulse"/>
+              <.icon id="crossedDataBase" class={classes("w-6 h-6 fill-gray-200 animation-pulse", %{"fill-red-600" => @status.writable == :no})}/>
             </div>
         <% end %>
 
@@ -99,17 +86,17 @@ defmodule ChatWeb.MainLive.Layout.DbStatus do
           <!-- Saving or downloading icon -->
           <%= if @status.flags[:backup] do %>
           <div>
-            <.icon id="backUp" class="w-6 h-6"/>
+            <.icon id="backUp" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
           </div>
           <% end %>
           <!-- Syncronization icon, like cirle arrows -->
           <%= if @status.flags[:replication] do %>
           <div class="w-[21px]">
-            <.icon id="replication" class="w-6 h-6"/>
+            <.icon id="replication" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
           </div>
            <% end %>
            <!-- <div class="h-[27px]">
-              <.icon id="car" class="w-6 h-6"/>
+              <.icon id="car" class={classes("w-6 h-6 fill-gray-200", %{"fill-red-600" => @status.writable == :no})}/>
             </div> -->
       </div>
     """

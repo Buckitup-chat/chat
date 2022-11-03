51b20d69846b6fccca29eb11c15d73bde4d0f7ad
Ruslan Kurbanov <ruslankurbanov@litslink.local>
2022-11-04 01:23:58 +0200

    ip configuration

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule ChatWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :chat
 
-  plug ChatWeb.Plugs.RedirectExtraTraffic
+  # plug ChatWeb.Plugs.RedirectExtraTraffic
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.

e509a1040095bab897d782e2ee563cf48953af79
Almir Sarajčić <almir.sarajcic@icloud.com>
2022-12-24 17:18:18 +0100

    Add the other meta tags as well

--- lib/chat_web/controllers/zip_controller.ex
+++ lib/chat_web/controllers/zip_controller.ex
@@ -71,7 +71,13 @@ defmodule ChatWeb.ZipController do
 
       exported_messages =
         Stream.concat([
-          [~S(<html><head><meta charset="utf-8" />)],
+          ["<html><head>"],
+          [~S(
+            <meta charset="utf-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Chat · BuckItUp</title>
+          )],
           ["<style>"],
           style_stream,
           ["</style></head><body>"],

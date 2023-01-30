aeacaf290836b4e337d0425e896216ef8f73ed39
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-01-30 21:51:04 +0200

    blowfish works

--- assets-fe/enigma.js
+++ assets-fe/enigma.js
@@ -11,17 +11,41 @@ const Enigma = {
   },
   cipher: "todo",
   decipher: function(b64_ciphered_data, b64_password) {
+    const pass_buffer = Buffer.from(b64_password, 'base64')
+    pass = new Uint8Array(pass_buffer.buffer, 8, 16)
+    key1 = new Uint8Array(pass_buffer.buffer, 0, 8)
+    key2 = new Uint8Array(pass_buffer.buffer, 24, 8)
 
 
-    let context = blf.key(key);
-    let plaintext = 'Same with CFB. Full blocks only!';
-    let ciphertext = blf.cfb(context, iv, Buffer.from(plaintext, 'utf8'));
-    let decrypted = blf.cfb(context, iv, ciphertext, true);
+
+    let context = blf.key(Buffer.from(pass));
+    //let plaintext = 'Same with CFB. Full blocks only!';
+    //let ciphertext = blf.cfb(context, iv, Buffer.from(plaintext, 'utf8'));
+    let deciphered = blf.cfb(context, Buffer.from(key1), Buffer.from(b64_ciphered_data, 'base64'), true);
+
+    return deciphered
   },
   encrypt: "todo",
-  decrypt: "todo"
+  decrypt: "todo",
+
+  base64_to_string: function(b64_data) {
+    return Buffer.from(b64_data, 'base64').toString();
+
+  },
+  base64_to_array: function(b64_data) {
+    const buffer = Buffer.from(b64_data, 'base64');
+
+    return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
+  },
+  array_to_base64: function(array) {
+    return Buffer.from(array).toString('base64');
+  },
+  string_to_base64: function(string) {
+    return Buffer.from(string).toString('base64');
+  }
 
 };
 
 window.enigma = Enigma;
-window.buffer = Buffer;
+window.Buffer = Buffer;
+window.blf = blf;

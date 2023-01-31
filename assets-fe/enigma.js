a1365c5784de4a04b24e66b14e087f3766a8b334
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-01-31 08:07:54 +0200

    FE decipher works

--- assets-fe/enigma.js
+++ assets-fe/enigma.js
@@ -3,13 +3,28 @@ const blf = require('blowfish-js');
 var Buffer = require('buffer/').Buffer  // note: the trailing slash is important!
 
 const Enigma = {
-  help: `All the functions work with Base64 encoded strings`,
+  help: `All the functions inputs are Base64 encoded strings`,
   hash: function(b64_data) {
     const shaObj = new jsSHA3("SHA3-256", "B64");
     shaObj.update(b64_data);
     return shaObj.getHash("B64");
   },
-  cipher: "todo",
+  cipher: function(b64_plain_data, b64_password) {
+    const pass_buffer = Buffer.from(b64_password, 'base64')
+    pass = Buffer.from(new Uint8Array(pass_buffer.buffer, 8, 16))
+    key1 = Buffer.from(new Uint8Array(pass_buffer.buffer, 0, 8))
+    key2 = Buffer.from(new Uint8Array(pass_buffer.buffer, 24, 8))
+
+    key = new Array(8)
+    for (i = 0; i < 8; i += 1) {
+      key[i] = key1[i] ^ key2[i]
+    }
+
+    let context = blf.key(Buffer.from(pass));
+    let ciphered = blf.cfb(context, Buffer.from(key), Buffer.from(b64_plain_data, 'base64'));
+
+    return ciphered
+  },
   decipher: function(b64_ciphered_data, b64_password) {
     const pass_buffer = Buffer.from(b64_password, 'base64')
     pass = Buffer.from(new Uint8Array(pass_buffer.buffer, 8, 16))
@@ -17,24 +32,12 @@ const Enigma = {
     key2 = Buffer.from(new Uint8Array(pass_buffer.buffer, 24, 8))
 
     key = new Array(8)
-
     for (i = 0; i < 8; i += 1) {
-      console.log(key1[i], key2[i], key1[i] ^ key2[i])
       key[i] = key1[i] ^ key2[i]
     }
 
-
-    console.log(pass_buffer)
-    console.log(key1, pass, key2)
-    console.log(key)
-
-
-
-
-    let context = blf.key(Buffer.from(key1));
-    //let plaintext = 'Same with CFB. Full blocks only!';
-    //let ciphertext = blf.cfb(context, iv, Buffer.from(plaintext, 'utf8'));
-    let deciphered = blf.cfb(context, Buffer.from(key1), Buffer.from(b64_ciphered_data, 'base64'), true);
+    let context = blf.key(Buffer.from(pass));
+    let deciphered = blf.cfb(context, Buffer.from(key), Buffer.from(b64_ciphered_data, 'base64'), true);
 
     return deciphered
   },

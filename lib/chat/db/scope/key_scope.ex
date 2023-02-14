4291026aa95d7ce68cc5187dc906459607a7b012
Almir Sarajčić <almir.sarajcic@icloud.com>
2023-02-14 21:03:04 +0100

    Complete keys scope

--- lib/chat/db/scope/key_scope.ex
+++ lib/chat/db/scope/key_scope.ex
@@ -7,14 +7,15 @@ defmodule Chat.Db.Scope.KeyScope do
 
   def get_keys(db, pub_keys_list) do
     hashes = hash_pubkeys(pub_keys_list)
-    snap = CubDB.snapshot(db)
 
-    MapSet.new()
-    |> add_full_users(snap)
-    |> add_dialogs(snap, hashes)
-    |> add_rooms(snap, hashes)
-    |> add_content(snap, hashes)
-    |> MapSet.to_list()
+    CubDB.with_snapshot(db, fn snap ->
+      MapSet.new()
+      |> add_full_users(snap)
+      |> add_dialogs(snap, hashes)
+      |> add_rooms(snap, hashes)
+      |> add_content(snap, hashes)
+      |> MapSet.to_list()
+    end)
   end
 
   defp add_full_users(acc_set, snap) do
@@ -34,7 +35,11 @@ defmodule Chat.Db.Scope.KeyScope do
 
     dialog_binhashes =
       dialog_keys
-      |> Enum.map(fn {:dialogs, hash} -> Utils.binhash(hash) end)
+      |> Enum.map(fn {:dialogs, hash} ->
+        hash
+        |> Utils.hash()
+        |> Utils.binhash()
+      end)
       |> MapSet.new()
 
     snap
@@ -64,29 +69,97 @@ defmodule Chat.Db.Scope.KeyScope do
   end
 
   defp add_content(acc_set, snap, {_, hashes}) do
-    IO.inspect(hashes, label: :hashes)
+    file_index =
+      snap
+      |> db_keys_stream({:file_index, 0, 0, 0}, {:"file_index\0", 0, 0, 0})
+      |> Stream.filter(fn {:file_index, reader_hash, _file_key, _message_id} ->
+        MapSet.member?(hashes, reader_hash)
+      end)
+      |> MapSet.new()
+
+    file_keys =
+      file_index
+      |> Enum.map(fn {:file_index, _reader_hash, file_key, _message_id} -> file_key end)
+      |> MapSet.new()
+
+    file_chunks =
+      snap
+      |> db_keys_stream({:file_chunk, 0, 0, 0}, {:"file_chunk\0", 0, 0, 0})
+      |> Stream.filter(fn {:file_chunk, file_key, _chunk_start, _chunk_end} ->
+        MapSet.member?(file_keys, file_key)
+      end)
+      |> MapSet.new()
 
     files =
       snap
       |> db_keys_stream({:file, 0}, {:"file\0", 0})
-      |> Stream.filter(fn {:file, key} ->
-        IO.inspect(key, label: :file_check)
-        MapSet.member?(hashes, key)
+      |> Stream.filter(fn {:file, file_key} ->
+        MapSet.member?(file_keys, file_key)
       end)
       |> MapSet.new()
 
-    memo =
+    chunk_keys =
+      snap
+      |> db_keys_stream(
+        {:chunk_key, {:file_chunk, 0, 0, 0}},
+        {:chunk_key, {:"file_chunk\0", 0, 0, 0}}
+      )
+      |> Stream.filter(fn {:chunk_key, {:file_chunk, file_key, _chunk_start, _chunk_end}} ->
+        MapSet.member?(file_keys, file_key)
+      end)
+      |> MapSet.new()
+
+    memo_index =
+      snap
+      |> db_keys_stream({:memo_index, 0, 0}, {:"memo_index\0", 0, 0})
+      |> Stream.filter(fn {:memo_index, reader_hash, _memo_key} ->
+        MapSet.member?(hashes, reader_hash)
+      end)
+      |> MapSet.new()
+
+    memo_keys =
+      memo_index
+      |> Enum.map(fn {:memo_index, _reader_hash, key} -> key end)
+      |> MapSet.new()
+
+    memos =
       snap
       |> db_keys_stream({:memo, 0}, {:"memo\0", 0})
       |> Stream.filter(fn {:memo, key} ->
-        IO.inspect(key, label: :memo_check)
-        MapSet.member?(hashes, key)
+        MapSet.member?(memo_keys, key)
+      end)
+      |> MapSet.new()
+
+    room_invite_index =
+      snap
+      |> db_keys_stream({:room_invite_index, 0, 0}, {:"room_invite_index\0", 0, 0})
+      |> Stream.filter(fn {:room_invite_index, reader_hash, _invite_key} ->
+        MapSet.member?(hashes, reader_hash)
+      end)
+      |> MapSet.new()
+
+    room_invite_keys =
+      room_invite_index
+      |> Enum.map(fn {:room_invite_index, _reader_hash, invite_key} -> invite_key end)
+      |> MapSet.new()
+
+    room_invites =
+      snap
+      |> db_keys_stream({:room_invite, 0}, {:"room_invite\0", 0})
+      |> Stream.filter(fn {:room_invite, invite_key} ->
+        MapSet.member?(room_invite_keys, invite_key)
       end)
       |> MapSet.new()
 
     acc_set
+    |> union_set(chunk_keys)
+    |> union_set(file_chunks)
+    |> union_set(file_index)
     |> union_set(files)
-    |> union_set(memo)
+    |> union_set(memo_index)
+    |> union_set(memos)
+    |> union_set(room_invite_index)
+    |> union_set(room_invites)
   end
 
   defp db_keys_stream(snap, min, max) do

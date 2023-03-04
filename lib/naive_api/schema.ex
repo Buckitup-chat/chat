d0686b5a12c61428ed927a90cda44baad633c1ac
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-03-04 15:01:23 +0200

    user endpoints definitions

--- lib/naive_api/schema.ex
+++ lib/naive_api/schema.ex
@@ -11,8 +11,62 @@ defmodule NaiveApi.Schema do
       resolve(fn _, _, _ -> {:ok, "Hello Wold"} end)
     end
 
-    field :keys, list_of(:key_pair) do
-      resolve(fn _, _, _ -> {:ok, []} end)
+    @desc "List all users excluding requesting one"
+    field :user_list, list_of(non_null(:card)) do
+      arg(:my_public_key, :public_key |> non_null)
+    end
+
+    # @desc """
+    # Reads chat messages.
+
+    # `before` and `amount` are in indexes. One index contains at least one message.
+    # If `before` omitted, most recent messaages will be returned.
+    # If `amount` omitted, messages in 20 indexes will be returned.
+    # """
+    # field :room_chat, list_of(:message |> non_null) do
+    #   arg(:peer_public_key, non_null(:public_key))
+    #   arg(:my_keypair, non_null(:input_key_pair))
+    #   arg(:before, :integer)
+    #   arg(:amount, :integer)
+    # end
+
+    # @desc """
+    # Reads room messages.
+
+    # `before` and `amount` are in indexes. One index contains at least one message.
+    # If `before` omitted, most recent messaages will be returned.
+    # If `amount` omitted, messages in 20 indexes will be returned.
+    # """
+    # field :room_read, list_of(:message |> non_null) do
+    #   arg(:room_keypair, non_null(:input_key_pair))
+    #   arg(:my_keypair, non_null(:input_key_pair))
+    #   arg(:before, :integer)
+    #   arg(:amount, :integer)
+    # end
+  end
+
+  mutation do
+    @desc """
+    Registers new user in the system. Providing unique keypair.
+
+    There is no uniqeness restriction on the name.
+    The system references users by their public_key 
+    """
+    field :user_sign_up, non_null(:identity) do
+      arg(:name, non_null(:string))
+    end
+
+    @desc """
+    Sends text message in the room.
+
+    There is no limit on `text` length.
+    `timestamp` is unixtime in seconds. Will use system time if `timestamp` omitted.  
+    """
+    field :room_send_text, non_null(:message_reference) do
+      arg(:room_keypair, non_null(:input_key_pair))
+      arg(:my_keypair, non_null(:input_key_pair))
+      arg(:text, :string |> non_null)
+      arg(:timestamp, :integer)
     end
   end
 end
--- lib/naive_api/schema/types.ex
+++ lib/naive_api/schema/types.ex
@@ -4,8 +4,98 @@ defmodule NaiveApi.Schema.Types do
   """
   use Absinthe.Schema.Notation
 
+  alias NaiveApi.Types.Bitstring
+
+  ###################
+  #  Scalars
+  ###################
+
+  scalar :private_key, description: "Private key representation" do
+    parse(&Bitstring.parse_33/1)
+    serialize(&Bitstring.serialize_33/1)
+  end
+
+  scalar :public_key, description: "Public key representation" do
+    parse(&Bitstring.parse_32/1)
+    serialize(&Bitstring.serialize_32/1)
+  end
+
+  scalar :file_key, description: "File key" do
+    parse(&Bitstring.parse_32/1)
+    serialize(&Bitstring.serialize_32/1)
+  end
+
+  scalar :bitstring, description: "Any binary data" do
+    parse(&Bitstring.parse/1)
+    serialize(&Bitstring.serialize/1)
+  end
+
+  ###################
+  #  Objects
+  ###################
+
+  @desc "Key pair"
   object :key_pair do
-    field(:private_key, :string)
-    field(:public_key, :string)
+    field(:private_key, non_null(:private_key))
+    field(:public_key, non_null(:public_key))
+  end
+
+  @desc "User or Room Identity"
+  object :identity do
+    field(:name, :string |> non_null)
+    field(:keys, :key_pair |> non_null)
+  end
+
+  @desc "User or Room card"
+  object :card do
+    field(:name, :string |> non_null)
+    field(:public_key, :public_key |> non_null)
+  end
+
+  @desc "Message reference"
+  object :message_reference do
+    field(:index, :integer |> non_null)
+    field(:id, :id |> non_null)
+  end
+
+  # enum :file_content_type do
+  #   value(:file)
+  #   value(:image)
+  #   value(:video)
+  #   value(:audio)
+  # end
+
+  # object :room_invite_content do
+  #   field(:room_card, :card |> non_null)
+  #   field(:invitation, :bitstring |> non_null)
+  # end
+
+  # object :file_content do
+  #   field(:url, :string |> non_null)
+  #   field(:type, :file_content_type |> non_null)
+  #   field(:size_bytes, :integer |> non_null)
+  #   field(:initial_name, :string |> non_null)
+  # end
+
+  # union :content do
+  #   types([:file_content, :room_invite_content, :string])
+  # end
+
+  # object :msg do
+  #   # field(:author, :card |> not_null)
+  #   # field(:content, :content |> not_null)
+  #   # field(:timestamp, :integer |> not_null)
+  #   # field(:index, :integer |> not_null)
+  #   field(:id, not_null(:id))
+  # end
+
+  ###################
+  #  Input objects
+  ###################
+
+  @desc "Key pair"
+  input_object :input_key_pair do
+    field(:private_key, non_null(:private_key))
+    field(:public_key, non_null(:public_key))
   end
 end

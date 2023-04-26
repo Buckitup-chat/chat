ed3b6b2b48de3cb2eb772cbf4862348b6871187c
roman-khomenko-dev <roman.khomenko@litslink.com>
2023-04-26 21:41:19 +0300

    test:  quality check for key share

---
+++ test/chat/key_share_test.exs
@@ -0,0 +1,31 @@
+defmodule Chat.KeyShareTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  alias Chat.Card
+  alias Chat.KeyShare
+  alias Chat.Rooms
+  alias Chat.User
+
+  describe "key share" do
+    setup do
+      me = "Root" |> User.login()
+      rooms = [me |> Rooms.add("All here") |> elem(0)]
+      users = ["Kevin", "John", "Mike", "Liza"]
+      user_cards = Enum.map(users, &User.login/1) |> Enum.map(&Card.from_identity/1)
+      shares = KeyShare.generate_key_shares({me, rooms, user_cards})
+      {:ok, me: me, rooms: rooms, users: users, user_cards: user_cards, shares: shares}
+    end
+
+    test "generate_key_shares/1", %{me: me, rooms: rooms, user_cards: user_cards} do
+      key_shares = KeyShare.generate_key_shares({me, rooms, user_cards})
+      assert Enum.count(key_shares) == Enum.count(user_cards)
+      assert Enum.all?(key_shares, &Map.has_key?(&1, :key))
+    end
+
+    test "send_shares/2", %{me: me, shares: shares} do
+      time_offset = 3
+      assert shares |> KeyShare.send_shares({me, time_offset}) == :ok
+    end
+  end
+end

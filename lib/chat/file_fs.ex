ed47f19a4c3e3a835559b3505f64032a811bb734
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-06-07 21:17:28 +0300

    code fix

--- lib/chat/file_fs.ex
+++ lib/chat/file_fs.ex
@@ -103,12 +103,6 @@ defmodule Chat.FileFs do
     end
   end
 
-  def has_file?({_, _, _} = keys, prefix \\ nil) do
-    keys
-    |> file_path(build_path(prefix))
-    |> File.exists?()
-  end
-
   ##
   ##   Implementations
   ##

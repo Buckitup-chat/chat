5be64ff1f486f00430c02a943f48d1b19c864a20
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-10-03 18:07:09 +0300

    528: simplify classes

--- lib/chat_web/live/main_live/layout/card.ex
+++ lib/chat_web/live/main_live/layout/card.ex
@@ -62,7 +62,7 @@ defmodule ChatWeb.MainLive.Layout.Card do
         <div class={@name_style}><%= @card.name %></div>
         <tt class={"ml-1 #{@hash_style}"}>[<%= Enigma.short_hash(@card) %>]</tt>
       <% else %>
-        <tt class={"#{@hash_style}"}>[<%= Enigma.short_hash(@card) %>]</tt>
+        <tt class={@hash_style}>[<%= Enigma.short_hash(@card) %>]</tt>
         <div class={"ml-1 #{@name_style}"}><%= @card.name %></div>
       <% end %>
       <%= if @is_me? do %>

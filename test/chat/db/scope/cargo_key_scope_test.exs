dcedd7bdeb69964d5099a93f23e2b54840a4e93b
roman-khomenko-dev <roman.khomenko@litslink.com>
2023-12-08 14:13:41 +0200

    test: correcting of cargo room invitation check

--- test/chat/db/scope/cargo_key_scope_test.exs
+++ test/chat/db/scope/cargo_key_scope_test.exs
@@ -12,30 +12,43 @@ defmodule Chat.Db.Scope.CargoKeyScopeTest do
 
   test "room and invitations in dialogs should be selected" do
     {bot_key, room_key} = generate_user_with_dialogs_content_and_invite_in_room()
-    assert_keys_for_cargo_keys(room_key, [bot_key])
+    assert_keys_for_cargo_keys(room_key, [bot_key], 1)
   end
 
-  test "room invitation should be found in dialog of checkpoint and cargo user by only checkpoints key" do
-    {admin, john, user} =
-      {User.login("admin"), User.login("John"), "User" |> User.login() |> register_and_put_user()}
+  test "room invitation should be found in dialog of operator and user by only checkpoints key" do
+    # Operator create checkpoints and save them to admin db
+    # Then invite them to Cargo room
+    # Operator create user C and invite him to Cargo room
+    # Check that cargo keys scope contain invites to checkpoints and user C by only checkpoints key
 
-    [checkpoint1_key, _checkpoint2_key] =
+    {admin, john, operator} =
+      {User.login("admin"), User.login("John"),
+       "Operator" |> User.login() |> register_and_put_user()}
+
+    [checkpoint1_key, checkpoint2_key] =
       generate_checkpoints([admin, john])
 
-    {_dialogs, room_key} = generate_dialogs_and_cargo_room_invite([admin, john], user)
+    room_identity = generate_cargo_room(operator)
+    room_key = Identity.pub_key(room_identity)
+    _dialogs = generate_dialogs_and_cargo_room_invite([admin, john], operator, room_identity)
 
-    assert_keys_for_cargo_keys(room_key, [checkpoint1_key])
+    user_c = User.login("UserC")
+    register_and_put_user(user_c)
+
+    generate_dialogs_and_cargo_room_invite([user_c], operator, room_identity)
+
+    assert_keys_for_cargo_keys(room_key, [checkpoint1_key, checkpoint2_key], 3)
   end
 
-  defp assert_keys_for_cargo_keys(room_key, checkpoint_keys) do
+  defp assert_keys_for_cargo_keys(room_key, checkpoint_keys, expected_count) do
     Process.sleep(100)
     keys = KeyScope.get_cargo_keys(Chat.Db.db(), room_key, checkpoint_keys)
 
     expected_keys = %{
-      dialog_message: 1,
-      dialogs: 1,
-      room_invite: 1,
-      room_invite_index: 1,
+      dialog_message: expected_count,
+      dialogs: expected_count,
+      room_invite: expected_count,
+      room_invite_index: expected_count,
       rooms: 1
     }
 
@@ -78,18 +91,18 @@ defmodule Chat.Db.Scope.CargoKeyScopeTest do
     identity |> tap(&User.register/1) |> tap(&User.UsersBroker.put/1)
   end
 
-  defp generate_dialogs_and_cargo_room_invite([_checkpoint1, _checkpoint2] = checkpoints, user) do
-    {room_identity, _room} = Rooms.add(user, "New Cargo room", :cargo)
-    room_key = Identity.pub_key(room_identity)
+  defp generate_cargo_room(user) do
+    {room_identity, _room} = Rooms.add(user, "Cargo room", :cargo)
+    room_identity
+  end
 
+  defp generate_dialogs_and_cargo_room_invite(checkpoints, user, room_identity) do
     dialogs =
       for checkpoint <- checkpoints,
           into: [],
           do: Dialogs.find_or_open(user, checkpoint |> Chat.Card.from_identity())
 
     Enum.each(dialogs, fn dialog -> create_and_add_room_invite(room_identity, dialog, user) end)
-
-    {dialogs, room_key}
   end
 
   defp create_and_add_room_invite(room_identity, dialog, user) do

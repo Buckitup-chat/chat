d57678bf5821a4ec2cdbc9e2091e20f50e1d8891
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-12-09 10:55:18 +0200

    556: unstable sync UI

--- lib/chat_web/live/main_live/page/admin_panel_router.ex
+++ lib/chat_web/live/main_live/page/admin_panel_router.ex
@@ -59,11 +59,17 @@ defmodule ChatWeb.MainLive.Page.AdminPanelRouter do
       {:updated_wifi_settings, _} ->
         socket |> AdminPanel.confirm_wifi_updated()
 
-        {:lan_ip, ip} -> AdminPanel.update_lan_settings(ip: ip)
-      {:lan_profile, profile} -> AdminPanel.update_lan_settings(profile: profile)
-      {:lan_known_profiles, list} -> AdminPanel.update_lan_settings(known_profiles: list)
+      {:lan_ip, ip} ->
+        AdminPanel.update_lan_settings(ip: ip)
+        socket
 
+      {:lan_profile, profile} ->
+        AdminPanel.update_lan_settings(profile: profile)
+        socket
 
+      {:lan_known_profiles, list} ->
+        AdminPanel.update_lan_settings(known_profiles: list)
+        socket
 
       {:device_log, log} ->
         socket |> AdminPanel.render_device_log(log)

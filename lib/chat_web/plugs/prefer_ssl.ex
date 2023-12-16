c491cb148ef8c5921a2c278922178d0e0141d49e
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-12-16 19:51:57 +0200

    556: lan accessible in both modes

--- lib/chat_web/plugs/prefer_ssl.ex
+++ lib/chat_web/plugs/prefer_ssl.ex
@@ -6,10 +6,21 @@ defmodule ChatWeb.Plugs.PreferSSL do
   def init(opts), do: opts
 
   if Application.compile_env(:chat, ChatWeb.Endpoint)[:https] do
-    def call(conn, _) do
-      conn
-      |> Phoenix.Controller.redirect(external: ~p"https://#{conn.host}")
+    def call(%{scheme: :https} = conn, _), do: conn
+    def call(conn, _), do: redirect(conn)
+
+    defp redirect(conn) do
+      ["https://", conn.host, conn.request_path, may_be_query_string(conn)]
+      |> Enum.reject(&is_nil/1)
+      |> Enum.join()
+      |> then(&Phoenix.Controller.redirect(conn, external: &1))
       |> Plug.Conn.halt()
+    end
+
+    defp may_be_query_string(conn) do
+      if conn.query_string != "" do
+        "?#{conn.query_string}"
+      end
     end
   else
     def call(conn, _), do: conn

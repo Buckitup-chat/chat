d8de30334e5e7b97a27218386e0e018860c737d7
Serhii Lukianov <serhii.lukianov@litslink.com>
2023-12-30 20:02:17 +0200

    572: fix supervision

--- lib/chat/network_synchronization/supervisor.ex
+++ lib/chat/network_synchronization/supervisor.ex
@@ -21,11 +21,12 @@ defmodule Chat.NetworkSynchronization.Supervisor do
     supervisor_name = Keyword.fetch!(opts, :supervisor_name)
     dynamic_name = Keyword.get(opts, :dynamic_name, Module.concat(supervisor_name, Dynamic))
     registry_name = Keyword.get(opts, :registry_name, Module.concat(supervisor_name, Registry))
+    detector_name = Keyword.get(opts, :detector_name, Module.concat(supervisor_name, Detector))
 
     children = [
       {DynamicSupervisor, name: dynamic_name, strategy: :one_for_one},
       {Registry, name: registry_name, keys: :unique},
-      LanDetector
+      {LanDetector, name: detector_name}
     ]
 
     Store.init()

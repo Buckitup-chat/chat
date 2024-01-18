0ba1eed68f913e724cc75b5062fd079221e589b1
Serhii Lukianov <serhii.lukianov@litslink.com>
2024-01-18 21:28:42 +0200

    425: zerotier UI tested

--- lib/chat_web/live/main_live/admin/zerotier_settings.ex
+++ lib/chat_web/live/main_live/admin/zerotier_settings.ex
@@ -51,7 +51,7 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
 
   def status(assigns) do
     ~H"""
-    <section class="my-2">
+    <section class="t-status my-2">
       <label class="text-black/50"> Status: </label>
       <%= if is_map(@status) do %>
         <.status_line
@@ -82,7 +82,7 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
 
   def networks_list(assigns) do
     ~H"""
-    <section class="my-2">
+    <section class="t-networks my-2">
       <label class="text-black/50"> Networks: </label>
       <%= if is_list(@list) do %>
         <%= for network <- @list do %>
@@ -106,7 +106,7 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
     ~H"""
     <section id={"zerotier-network-#{@id}"} class="my-2 flex space-x-2 justify-between">
       <button
-        class="flip hidden items-center justify-center rounded-md px-2 border bg-red-500 border-black/10 mr-1"
+        class="flip hidden items-center justify-center rounded-md px-2 border bg-red-500 border-black/10 mr-1 t-leave-network"
         phx-click={
           JS.toggle(to: "#zerotier-network-#{@id} .flip")
           |> JS.push("leave_network", target: @target)
@@ -140,7 +140,7 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
     ~H"""
     <div id="zerotier-add-network">
       <button
-        class="flip"
+        class="flip t-open-new-network-form"
         phx-click={
           JS.toggle(to: "#zerotier-add-network .flip")
           |> JS.focus(to: "#zerotier-add-network input")
@@ -179,13 +179,13 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
     """
   end
 
-  defp parse_info_response(json) do
+  def parse_info_response(json) do
     json
     |> Jason.decode!()
     |> Map.take(["address", "online", "version"])
   end
 
-  defp parse_networks_response(json) do
+  def parse_networks_response(json) do
     json
     |> Jason.decode!()
     |> Enum.map(fn net ->
@@ -198,108 +198,15 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
     end)
   end
 
-  def info_json() do
-    """
-      {
-       "address": "56bcbb9d9c",
-       "clock": 1704914749230,
-       "config": {
-        "settings": {
-         "allowTcpFallbackRelay": true,
-         "forceTcpRelay": false,
-         "listeningOn": [
-          "192.168.25.1/48129",
-          "192.168.0.247/48129",
-          "192.168.25.1/9993",
-          "192.168.0.247/9993",
-          "192.168.25.1/43308",
-          "192.168.0.247/43308"
-         ],
-         "portMappingEnabled": true,
-         "primaryPort": 9993,
-         "secondaryPort": 43308,
-         "softwareUpdate": "disable",
-         "softwareUpdateChannel": "release",
-         "surfaceAddresses": [
-          "188.191.238.67/35219",
-          "188.191.238.67/48129",
-          "188.191.238.67/43308"
-         ],
-         "tertiaryPort": 48129
-        }
-       },
-       "online": true,
-       "planetWorldId": 149604618,
-       "planetWorldTimestamp": 1644592324813,
-       "publicIdentity": "56bcbb9d9c:0:854b6ddfa98fe769a73f2d09c4606f3694645f66e26bdf32c43af7149cf2b402e588294400bc645ce902e3c1cce91f6b716db0f09e349e69d7506a9751e3c838",
-       "tcpFallbackActive": false,
-       "version": "1.12.2",
-       "versionBuild": 0,
-       "versionMajor": 1,
-       "versionMinor": 12,
-       "versionRev": 2
-      }
-    """
-  end
-
-  def networks_json do
-    """
-        [
-         {
-          "allowDNS": false,
-          "allowDefault": false,
-          "allowGlobal": false,
-          "allowManaged": true,
-          "assignedAddresses": [
-           "10.147.20.50/24"
-          ],
-          "bridge": false,
-          "broadcastEnabled": true,
-          "dhcp": false,
-          "dns": {
-           "domain": "",
-           "servers": []
-          },
-          "id": "1d71939404f0a869",
-          "mac": "6a:fe:4c:bf:09:0f",
-          "mtu": 2800,
-          "multicastSubscriptions": [
-           {
-            "adi": 0,
-            "mac": "01:00:5e:00:00:fb"
-           },
-           {
-            "adi": 0,
-            "mac": "33:33:00:00:00:01"
-           }
-          ],
-          "name": "salseeg_first",
-          "netconfRevision": 2,
-          "nwid": "1d71939404f0a869",
-          "portDeviceName": "ztrf2wgpdn",
-          "portError": 0,
-          "routes": [
-           {
-            "flags": 0,
-            "metric": 0,
-            "target": "10.147.20.0/24",
-            "via": null
-           }
-          ],
-          "status": "OK",
-          "type": "PRIVATE"
-         }
-        ]
-    """
-  end
-
   defp request_status do
     pid = self()
 
     call_command(:info,
       ok: fn response ->
+        # coveralls-ignore-start
         status = response |> parse_info_response
         send_update(pid, ZerotierSettings, id: :zerotier_settings, status: status)
+        # coveralls-ignore-stop
       end
     )
   end
@@ -307,11 +214,20 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
   defp request_networks do
     pid = self()
 
-    call_command(:list_networks,
-      ok: fn response ->
-        networks = response |> parse_networks_response
-        send_update(pid, ZerotierSettings, id: :zerotier_settings, list: networks)
-      end
+    [0, 700, 1500, 5000]
+    |> Enum.each(
+      &Task.start(fn ->
+        Process.sleep(&1)
+
+        call_command(:list_networks,
+          ok: fn response ->
+            # coveralls-ignore-start
+            networks = response |> parse_networks_response
+            send_update(pid, ZerotierSettings, id: :zerotier_settings, list: networks)
+            # coveralls-ignore-stop
+          end
+        )
+      end)
     )
   end
 
@@ -328,8 +244,10 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
       Phoenix.PubSub.broadcast(Chat.PubSub, @topic, {command, self()})
 
       receive do
+        # coveralls-ignore-next-line
         _ -> :ok
       after
+        # coveralls-ignore-next-line
         5000 -> :timeout
       end
     end)
@@ -343,10 +261,13 @@ defmodule ChatWeb.MainLive.Admin.ZerotierSettings do
       Phoenix.PubSub.broadcast(Chat.PubSub, @topic, {command, self()})
 
       receive do
+        # coveralls-ignore-start
         {^command, {:ok, response}} -> ok.(response)
         {^command, {:error, response}} -> error.(response)
         _ -> :ignore
+        # coveralls-ignore-stop
       after
+        # coveralls-ignore-next-line
         5000 -> :timeout
       end
     end)

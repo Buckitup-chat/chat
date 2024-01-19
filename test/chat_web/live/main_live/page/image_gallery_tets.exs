55105392cf5d2338f0009e2126d88f4314e47fae
roman-khomenko-dev <roman.khomenko@litslink.com>
2024-01-19 13:02:48 +0200

    fix: gallery test preparation

--- test/chat_web/live/main_live/page/image_gallery_tets.exs
+++ test/chat_web/live/main_live/page/image_gallery_tets.exs
@@ -1,14 +1,29 @@
 defmodule ChatWeb.MainLive.Page.ImageGalleryTest do
-    use ChatWeb.ConnCase, async: false
+  use ChatWeb.ConnCase, async: false
 
-    import ChatWeb.LiveTestHelpers
-    import Phoenix.LiveViewTest
+  import ChatWeb.LiveTestHelpers
+  import Phoenix.LiveViewTest
 
-    alias ChatWeb.MainLive.Page.ImageGallery
+  alias ChatWeb.MainLive.Page.ImageGallery
 
-    describe "image gallery" do
-        
-        test "open gallery after transition from intive to room" do
-            
-        end
+  describe "image gallery" do
+    setup [:create_initial_state]
+    test "open gallery after transition from intive to room", %{conn: conn, state: state} do
+      %{}
+      |> create_users(2)
     end
+
+    defp create_users(conn, number) do
+      Enum.reduce(1..number, %{conn: conn, users: []}, fn _, acc ->
+        {conn, user} = IndexTest.create_user(acc.conn)
+        Map.put(acc, :users, [user | acc.users])
+      end)
+    end
+  
+    defp create_initial_state(_) do
+      # Set up the initial state by creating users
+      {conn, users_state} = create_users(2)
+      %{conn: conn, state: users_state}
+    end
+  end
+end

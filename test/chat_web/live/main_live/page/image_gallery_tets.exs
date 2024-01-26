406b406247ea1f44c3713909b3281ed07457789d
roman-khomenko-dev <roman.khomenko@litslink.com>
2024-01-26 11:30:56 +0200

    test: add room invitation acceptance

--- test/chat_web/live/main_live/page/image_gallery_tets.exs
+++ test/chat_web/live/main_live/page/image_gallery_tets.exs
@@ -16,22 +16,22 @@ defmodule ChatWeb.MainLive.Page.ImageGalleryTest do
       second_person: second_person
     } do
       persons = [first_person, second_person]
-      {_sender, recipient} = extract_participants(persons)
+      {sender, recipient} = extract_participants(persons)
 
       %{}
       |> init_views(persons)
       |> create_room_and_upload_image()
       |> send_room_invitation()
       |> open_dialog_and_upload_image(recipient: recipient)
-
-      # |> accept_room_invitation(from: sender)
+      |> accept_room_invitation(from: sender)
     end
 
-    defp accept_room_invitation(%{views: [_ | view] = _views} = context, from: user) do
+    defp accept_room_invitation(%{views: [_, view] = _views} = context, from: sender) do
       %{view: view}
-      |> open_dialog(user)
-
-      # TODO: write room invitation acceptance
+      |> open_dialog(sender)
+      |> update_context_view(context)
+      |> select_room_invitation()
+      |> render_click()
 
       context
     end
@@ -67,10 +67,10 @@ defmodule ChatWeb.MainLive.Page.ImageGalleryTest do
     end
 
     defp upload_image(%{views: [view_1 | _] = _views} = context, source) do
-      with %{entry: entry, socket: _socket_1} <- start_upload(%{view: view_1}),
-           entry <- %{entry | done?: true, progress: 100},
+      with %{entry: entry, socket: _socket} <- start_upload(%{view: view_1}),
+           _entry <- %{entry | done?: true, progress: 100},
            :ok <- ChangeTracker.await() do
-        Map.put(context, "#{source}_image", entry)
+        Map.put(context, "#{source}_image" |> String.to_atom(), true)
       else
         _ -> context
       end
@@ -85,13 +85,18 @@ defmodule ChatWeb.MainLive.Page.ImageGalleryTest do
     defp init_views(context, persons),
       do: persons |> extract_views() |> put_context_value({:views, context})
 
-    defp update_context_view(%{view: view}, %{views: views} = context),
-      do: views |> List.replace_at(0, view) |> put_context_value({:views, context})
+    defp update_context_view(%{view: view}, %{views: views} = context) do
+      views
+      |> List.replace_at(which_view_update?(context), view)
+      |> put_context_value({:views, context})
+    end
 
     defp put_context_value(value, {key, context}), do: Map.put(context, key, value)
 
     defp first_person(%{conn: _} = conn), do: [first_person: prepare_view(conn, "Bonnie")]
     defp second_person(%{conn: _} = conn), do: [second_person: prepare_view(conn, "Clyde")]
+
+    defp which_view_update?(context), do: if(Map.has_key?(context, :dialog_image), do: 1, else: 0)
 
     defp extract_participants(persons) do
       persons
@@ -106,5 +111,8 @@ defmodule ChatWeb.MainLive.Page.ImageGalleryTest do
 
     defp extract_views(persons) when is_list(persons),
       do: Enum.map(persons, fn %{view: view} -> view end)
+
+    defp select_room_invitation(%{views: [_, view]} = _context),
+      do: element(view, "button", "Accept and Open")
   end
 end

0b57af20604c0dbb4b76e1bfe6a838b89947fd1e
Serhii Lukianov <serhii.lukianov@litslink.com>
2024-05-23 21:42:37 +0300

    571: plain editing

--- lib/chat/admin_room.ex
+++ lib/chat/admin_room.ex
@@ -108,4 +108,91 @@ defmodule Chat.AdminRoom do
 
   def store_media_settings(%MediaSettings{} = media_settings),
     do: AdminDb.put(:media_settings, media_settings)
+
+  def get_privacy_policy_text do
+    AdminDb.get(:privacy_policy_text) || hardcoded_privacy_policy()
+  end
+
+  defp hardcoded_privacy_policy do
+    """
+
+    <h1>Privacy Policy</h1>
+
+    <h2>1. Introduction</h2>
+    <p>BuckitUp respects your privacy. This Privacy Policy outlines our practices regarding data collection, use, and sharing.
+
+    <h2>2. No Data Collection</h2>
+    <p>BuckitUp does not collect any personal or usage data through its application.
+
+    <h2>3. No Data Sharing</h2>
+    <p>Since we do not collect any data, we do not share any personal information with third parties.
+
+    <h2>4. No Data Usage</h2>
+    <p>BuckitUp does not use any personal or usage data for any purposes.
+
+    <h2>5. Contact Us</h2>
+    <p>If you have any questions about this Privacy Policy, please contact us at <a href="mailto:buckitup.lv@gmail.com">buckitup.lv@gmail.com</a>.
+
+
+    <hr>
+
+    <h1>Terms & Conditions</h1>
+
+    <h2>Introduction</h2>
+    <p>Welcome to BuckitUp. By accessing or using our application, you agree to be bound by these Terms & Conditions. If you do not agree with any part of these terms, you must not use our application.</p>
+
+    <h2>Usage Terms</h2>
+    <h3>1. Acceptance of Terms</h3>
+    By downloading, installing, and using the application, you agree to comply with these Terms & Conditions and the GNU General Public License v3.0 (GPL v3.0). The full terms of the GPL v3.0 can be viewed <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">here</a>.
+
+    <h3>2. User Responsibilities</h3>
+    <ul>
+    <li>Account Registration: All user registration processes are conducted locally on your device. We do not store or manage any user data.</li>
+    <li>Data Management: Users are solely responsible for managing their data and ensuring its security.</li>
+    <li>Compliance: You must use the application in compliance with all applicable laws and regulations.</li>
+    </ul>
+
+    <h3>3. License and Restrictions</h3>
+    <p>You are granted a non-exclusive, non-transferable, revocable license to use the application for personal or internal business purposes, in accordance with these Terms & Conditions and the GPL v3.0. You agree not to:
+    <ul>
+    <li>Use the application for any illegal or unauthorized purpose.</li>
+    <li>Modify, adapt, hack, or reverse engineer the application, except as permitted under the GPL v3.0.</li>
+    <li>Reproduce, duplicate, copy, sell, trade, or resell the application, except as permitted under the GPL v3.0.</li>
+    </ul>
+
+    <h3>4. Intellectual Property</h3>
+    <p>All intellectual property rights in the application are owned by SIA BuckitUp, subject to the rights granted under the GPL v3.0.
+
+    <h3>5. Limitation of Liability</h3>
+    <p>To the fullest extent permitted by law, SIA BuckitUp shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses resulting from:
+    <ul>
+    <li>Your use or inability to use the application.</li>
+    <li>Any unauthorized access or use of your data.</li>
+    <li>Any content obtained from the application.</li>
+    </ul>
+
+    <h3>6. Disclaimer of Warranties</h3>
+    <p>The application is provided “as is” and “as available” without any warranties of any kind, either express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, or non-infringement.
+
+    <h3>7. Indemnification</h3>
+    <p>You agree to indemnify, defend, and hold harmless SIA BuckitUp, its officers, directors, employees, and agents, from and against any claims, liabilities, damages, losses, and expenses, including, without limitation, reasonable legal and accounting fees, arising out of or in any way connected with your access to or use of the application or your violation of these Terms & Conditions.
+
+    <h3>8. Governing Law</h3>
+    <p>These Terms & Conditions shall be governed by and construed in accordance with the laws of Latvia, without regard to its conflict of law principles.
+
+    <h3>9. Changes to Terms & Conditions</h3>
+    <p>We reserve the right to modify or replace these Terms & Conditions at any time. We will provide notice of any changes by updating the terms on our website. Your continued use of the application after any such changes constitutes your acceptance of the new Terms & Conditions.
+
+    <h3>10. Contact Information</h3>
+    <p>If you have any questions about these Terms & Conditions, please contact us at:
+    <ul>
+    <li>Email: <a href="mailto:buckitup.lv@gmail.com">"buckitup.lv@gmail.com</a></li>
+    <li>Address: Brāļu Kaudzīšu iela 2, 66, Riga LV-1082, Latvia</li>
+    </ul>
+
+    <p>By using our application, you acknowledge that you have read, understood, and agree to be bound by these Terms & Conditions and the GPL v3.0
+    """
+  end
+
+  def store_privacy_policy_text(text), do: AdminDb.put(:privacy_policy_text, text)
 end
--- lib/chat_web/live/main_live/admin/privacy_policy.ex
+++ lib/chat_web/live/main_live/admin/privacy_policy.ex
@@ -4,28 +4,67 @@ defmodule ChatWeb.MainLive.Admin.PrivacyPolicy do
   """
   use ChatWeb, :live_component
 
+  alias Phoenix.LiveView.JS
+  alias Chat.AdminRoom
+
   def mount(socket) do
     socket
     |> assign(:privacy_policy, load_privacy_policy())
     |> ok()
   end
 
+  def handle_event("save", %{"privacy-policy" => privacy_policy}, socket) do
+    text = privacy_policy |> String.trim()
+    save_privacy_policy(text)
+
+    socket
+    |> assign(:privacy_policy, text)
+    |> noreply()
+  end
+
   def render(assigns) do
     ~H"""
     <div id="privacy-policy-editor">
-      Put raw html here <textarea id="privacy-policy" name="privacy-policy">{@privacy_policy}</textarea>
-
-      <button phx-click="save" target={@myself}>Save</button>
+      <div>
+        <a
+          data-role="expand-button"
+          phx-click={
+            JS.add_class("w-[85vw]", to: "#privacy-policy-editor form")
+            |> JS.add_class("h-[70vh]", to: "#privacy-policy")
+            |> JS.hide(to: "#privacy-policy-editor [data-role=expand-button]")
+            |> JS.show(to: "#privacy-policy-editor [data-role=collapse-button]")
+          }
+        >
+          Expand
+        </a>
+        <a
+          data-role="collapse-button"
+          class="hidden"
+          phx-click={
+            JS.remove_class("w-[85vw]", to: "#privacy-policy-editor form")
+            |> JS.remove_class("h-[70vh]", to: "#privacy-policy")
+            |> JS.hide(to: "#privacy-policy-editor [data-role=collapse-button]")
+            |> JS.show(to: "#privacy-policy-editor [data-role=expand-button]")
+          }
+        >
+          Collapse
+        </a>
+      </div>
+      <form phx-submit="save" phx-target={@myself}>
+        <textarea id="privacy-policy" name="privacy-policy" class="w-full h-full"><%= @privacy_policy %></textarea>
+        <div>
+          <button>Save</button>
+        </div>
+      </form>
     </div>
     """
   end
 
   defp load_privacy_policy do
-    """
-    <h1>Privacy Policy</h1>
-
-    <p>Privacy Policy</p>
-    """
+    AdminRoom.get_privacy_policy_text()
   end
 
+  defp save_privacy_policy(privacy_policy) do
+    AdminRoom.store_privacy_policy_text(privacy_policy)
+  end
 end

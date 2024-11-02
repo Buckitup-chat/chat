73563ef8e478506d949f4a997f87c9111503c324
Serhii Lukianov <serhii.lukianov@litslink.com>
2024-11-02 18:08:42 +0200

    fix message date

--- assets/js/webcomponents/timestamp.js
+++ assets/js/webcomponents/timestamp.js
@@ -4,7 +4,7 @@ export default class Timestamp extends HTMLElement {
   }
 
   connectedCallback() {
-    const date = new Date(this.innerText * 1000);
+    const date = new Date(this.dataset.unixtime * 1000);
     const timeString = date.toLocaleTimeString();
     const dateString = date.toLocaleDateString();
     this.textContent = `${timeString} ${dateString}`;

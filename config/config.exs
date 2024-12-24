ded50577ab4ed403e242d78bdb35cf29be8cf8a7
Serhii Lukianov <serhii.lukianov@litslink.com>
2024-12-24 17:18:35 +0200

    node and esbuild update

--- config/config.exs
+++ config/config.exs
@@ -29,10 +29,10 @@ config :chat, Chat.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.14.0",
+  version: "0.24.2",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]

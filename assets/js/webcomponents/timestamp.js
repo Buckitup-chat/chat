85c25b995fbcf6eda7532e6d05e931ec6bce7bb6
Serhii Lukianov <serhii.lukianov@litslink.com>
2025-05-24 20:55:06 +0300

    chat: fix timestamp

--- assets/js/webcomponents/timestamp.js
+++ assets/js/webcomponents/timestamp.js
@@ -3,7 +3,27 @@ export default class Timestamp extends HTMLElement {
     super();
   }
 
+  // Define which attributes should be observed for changes
+  static get observedAttributes() {
+    return ['data-unixtime'];
+  }
+
+  // Called when the element is added to the DOM
   connectedCallback() {
+    this.updateTimestamp();
+  }
+
+  // Called when an observed attribute changes
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'data-unixtime' && oldValue !== newValue) {
+      this.updateTimestamp();
+    }
+  }
+
+  // Unified method to update the timestamp display
+  updateTimestamp() {
+    if (!this.dataset.unixtime) return;
+    
     const date = new Date(this.dataset.unixtime * 1000);
     const timeString = date.toLocaleTimeString();
     const dateString = date.toLocaleDateString();

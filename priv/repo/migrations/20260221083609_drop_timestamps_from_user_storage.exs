1480408abe01a4f2bbd2bb075eb848771add190e
s <salseeg@gmail.com>
2026-02-24 08:46:40 +0200

    pq: fix migration

--- priv/repo/migrations/20260221083609_drop_timestamps_from_user_storage.exs
+++ priv/repo/migrations/20260221083609_drop_timestamps_from_user_storage.exs
@@ -1,10 +1,16 @@
 defmodule Chat.Repo.Migrations.DropTimestampsFromUserStorage do
   use Ecto.Migration
 
-  def change do
-    alter table(:user_storage) do
-      remove :inserted_at
-      remove :updated_at
-    end
+  def up do
+    execute """
+    ALTER TABLE user_storage
+    DROP COLUMN IF EXISTS inserted_at,
+    DROP COLUMN IF EXISTS updated_at;
+    """
+  end
+
+  def down do
+    # Timestamps were never part of the original schema, so no need to re-add them
+    :ok
   end
 end
--- priv/repo/migrations/20260223105135_rename_user_storage_value_to_value_b64.exs
+++ priv/repo/migrations/20260223105135_rename_user_storage_value_to_value_b64.exs
@@ -1,7 +1,37 @@
 defmodule Chat.Repo.Migrations.RenameUserStorageValueToValueB64 do
   use Ecto.Migration
 
-  def change do
-    rename table(:user_storage), :value, to: :value_b64
+  def up do
+    execute """
+    DO $$
+    BEGIN
+      IF EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'user_storage' AND column_name = 'value'
+      ) AND NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'user_storage' AND column_name = 'value_b64'
+      ) THEN
+        ALTER TABLE user_storage RENAME COLUMN value TO value_b64;
+      END IF;
+    END $$;
+    """
+  end
+
+  def down do
+    execute """
+    DO $$
+    BEGIN
+      IF EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'user_storage' AND column_name = 'value_b64'
+      ) AND NOT EXISTS (
+        SELECT 1 FROM information_schema.columns
+        WHERE table_name = 'user_storage' AND column_name = 'value'
+      ) THEN
+        ALTER TABLE user_storage RENAME COLUMN value_b64 TO value;
+      END IF;
+    END $$;
+    """
   end
 end

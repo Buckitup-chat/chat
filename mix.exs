078dd859f88276b534f565ae35003bbddc4bfd55
s <salseeg@gmail.com>
2026-09-16 09:15:16 +0300

    update: split mix.exs

--- mix.exs
+++ mix.exs
@@ -7,23 +7,14 @@ defmodule Chat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
-      # compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      consolidate_protocols: Mix.env() != :dev,
       aliases: aliases(),
       deps: deps(),
-      test_coverage: [tool: ExCoveralls],
-      dialyzer: [plt_add_apps: [:ex_unit]],
-      releases: [
-        chat: [
-          version: build_version(),
-          applications: [chat: :permanent]
-        ]
-      ],
       listeners: [
         Phoenix.CodeReloader
       ]
     ]
+    |> more_project()
   end
 
   def cli do
@@ -36,49 +27,23 @@ defmodule Chat.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       mod: {Chat.Application, []},
-      extra_applications: [:logger, :runtime_tools, :curvy, :os_mon]
+      extra_applications: [:logger, :runtime_tools]
     ]
+    |> more_application()
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:logger_backends, "~> 1.0"},
-      {:tidewave, "~> 0.1", only: [:dev]},
-      {:igniter, "~> 0.5", only: [:dev, :test]},
-      # Chat deps
-      {:qr_code, "~> 2.2.1"},
-      # Using electric_cubdb instead of cubdb to avoid conflicts with Electric
-      {:electric_cubdb, "~> 2.0", override: true},
-      {:curvy, "~> 0.3.1"},
-      {:struct_access, "~> 1.1"},
-      {:uuid, "~> 1.1"},
-      {:uuidv7, "~> 0.2"},
-      {:ip, "~> 2.0"},
-      {:slipstream, "~> 1.1"},
-      {:onvif, github: "sergey-lukianov/onvif"},
-      {:keyx, "~> 0.4.1"},
-      {:combinatorics, "~> 0.1.0"},
-      {:timex, "~> 3.7"},
-      {:tzdata, "~> 1.1"},
-
-      # Phoenix
       {:phoenix, "~> 1.8"},
       {:phoenix_html, "~> 4.0"},
-      {:phoenix_html_helpers, "~> 1.0"},
-      {:phoenix_view, "~> 2.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "1.0.17"},
       {:phoenix_ecto, "~> 4.0"},
@@ -87,65 +52,106 @@ defmodule Chat.MixProject do
       {:postgrex, "~> 0.16"},
       {:plug_cowboy, "~> 2.5"},
       {:phoenix_live_dashboard, "~> 0.7"},
-      # {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:cors_plug, "~> 3.0"},
-
-      # Build tooling
-      {:live_vue, "~> 0.5"},
-      # {:esbuild, "~> 0.3", runtime: Mix.env() == :dev && Mix.target() == :host},
-      # {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.8", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.14", only: [:test]},
-      {:rewire, "~> 0.9", only: [:dev, :test]},
-      # {:live_isolated_component, "~> 0.8", only: [:dev, :test]},
-      # {:live_isolated_component, git: "https://github.com/Aratramba/live_isolated_component.git", branch: "upgrade-liveview-1.1", only: [:dev, :test]},
-      # {:live_isolated_component, git: "https://github.com/geofflane/live_isolated_component.git", branch: "chore/phoenix_18", only: [:dev, :test]},
-      {:live_isolated_component,
-       github: "sergey-lukianov/live_isolated_component", only: [:dev, :test]},
-      {:lazy_html, ">= 0.1.0", only: :test},
-
-      # other
-      {:absinthe, "~> 1.7"},
-      {:absinthe_plug, "~> 1.5"},
-      {:neuron, "~> 5.1"},
-      {:temp, "~> 0.4.7"},
-      {:floki, ">= 0.30.0", only: :test},
       {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:mock, "~> 0.3.0", only: :test},
-      {:zstream, "~> 0.6"},
-      {:ua_parser, github: "beam-community/ua_parser"},
-      {:httpoison, "~> 2.0"},
-      {:req, "~> 0.5"},
-      {:tesla, "~> 1.7"},
-
-      # ElectricSQL / Phoenix.Sync
-      {:electric, "~> 1.1"},
-      {:phoenix_sync, "~> 0.6.1"},
-      {:electric_client,
-       github: "salseeg/electric",
-       sparse: "packages/elixir-client",
-       branch: "buckitup-patches",
-       override: true},
-
-      # Internal
-      {:toolbox, github: "Buckitup-chat/toolbox"}
+      {:telemetry_poller, "~> 1.0"}
     ]
+    |> more_deps()
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      # setup: ["deps.get"],
-      # "assets.deploy": ["esbuild default --minify", "tailwind default --minify", "phx.digest"],
+      setup: ["deps.get"]
+    ]
+    |> more_aliases()
+  end
+
+  ################################
+  defp more_project(project) do
+    Keyword.merge(project,
+      consolidate_protocols: Mix.env() != :dev,
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: [plt_add_apps: [:ex_unit]],
+      releases: [
+        chat: [
+          version: build_version(),
+          applications: [chat: :permanent]
+        ]
+      ]
+    )
+  end
+
+  defp more_application(app) do
+    update_in(app, [:extra_applications], &(&1 ++ [:curvy, :os_mon]))
+  end
+
+  defp more_deps(deps) do
+    deps ++
+      [
+        {:logger_backends, "~> 1.0"},
+        {:tidewave, "~> 0.1", only: [:dev]},
+        {:igniter, "~> 0.5", only: [:dev, :test]},
+        # Chat deps
+        {:qr_code, "~> 2.2.1"},
+        # electric_cubdb instead of cubdb to avoid conflicts with Electric
+        {:electric_cubdb, "~> 2.0", override: true},
+        {:curvy, "~> 0.3.1"},
+        {:struct_access, "~> 1.1"},
+        {:uuid, "~> 1.1"},
+        {:uuidv7, "~> 0.2"},
+        {:ip, "~> 2.0"},
+        {:slipstream, "~> 1.1"},
+        {:onvif, github: "sergey-lukianov/onvif"},
+        {:keyx, "~> 0.4.1"},
+        {:combinatorics, "~> 0.1.0"},
+        {:timex, "~> 3.7"},
+        {:tzdata, "~> 1.1"},
+
+        # Phoenix extras
+        {:phoenix_html_helpers, "~> 1.0"},
+        {:phoenix_view, "~> 2.0"},
+        {:cors_plug, "~> 3.0"},
+
+        # Build tooling
+        {:live_vue, "~> 0.5"},
+        {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+        {:sobelow, "~> 0.8", only: [:dev, :test], runtime: false},
+        {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+        {:excoveralls, "~> 0.14", only: [:test]},
+        {:rewire, "~> 0.9", only: [:dev, :test]},
+        {:live_isolated_component,
+         github: "sergey-lukianov/live_isolated_component", only: [:dev, :test]},
+        {:lazy_html, ">= 0.1.0", only: :test},
+
+        # other
+        {:absinthe, "~> 1.7"},
+        {:absinthe_plug, "~> 1.5"},
+        {:neuron, "~> 5.1"},
+        {:temp, "~> 0.4.7"},
+        {:floki, ">= 0.30.0", only: :test},
+        {:mock, "~> 0.3.0", only: :test},
+        {:zstream, "~> 0.6"},
+        {:ua_parser, github: "beam-community/ua_parser"},
+        {:httpoison, "~> 2.0"},
+        {:req, "~> 0.5"},
+        {:tesla, "~> 1.7"},
+
+        # ElectricSQL / Phoenix.Sync
+        {:electric, "~> 1.1"},
+        {:phoenix_sync, "~> 0.6.1"},
+        {:electric_client,
+         github: "salseeg/electric",
+         sparse: "packages/elixir-client",
+         branch: "buckitup-patches",
+         override: true},
+
+        # Internal
+        {:toolbox, github: "Buckitup-chat/toolbox"}
+      ]
+  end
+
+  defp more_aliases(aliases) do
+    Keyword.merge(aliases,
       setup: ["deps.get", "assets.setup", "assets.build"],
       "assets.setup": ["cmd --cd assets npm install"],
       "assets.build": [
@@ -155,7 +161,7 @@ defmodule Chat.MixProject do
         "cmd --cd assets npm run build",
         "phx.digest"
       ]
-    ]
+    )
   end
 
   defp build_version do
